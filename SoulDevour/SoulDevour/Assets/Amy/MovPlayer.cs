a475064635cabe7a0992d9e93af6a5ef595e68af
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2022-02-02 10:36:45 +0100

    camera save

--- SoulDevour/SoulDevour/Assets/Amy/MovPlayer.cs
+++ SoulDevour/SoulDevour/Assets/Amy/MovPlayer.cs
@@ -5,16 +5,122 @@ using UnityEngine;
 public class MovPlayer : MonoBehaviour
 {
 
+    public Rigidbody playerController;
+    public Transform cam;
+    float turnSmoothVelocity;
+    public float turnSmoothTime = 0.1f;
+    bool isGrounded;
+    Vector3 moveVector;
+    float verticalVelosity;
+    float targetAngle;
 
-    // Start is called before the first frame update
-    void Start()
+    bool done = true;
+    bool changed = false;
+
+    public float speedForMove = 5;
+
+
+    private void Update()
     {
-        
+        Move(speedForMove);
+    }
+    private void FixedUpdate()
+    {
+
+        //SetRotation();
+       
+        //if (moveVector.x.Equals(Vector3.zero.x) && moveVector.z.Equals(Vector3.zero.z))
+        //{
+        //    //idle
+        //    SetRotation();
+        //}
+        //else if ((Input.GetButton("Left Shift") || Input.GetButtonDown("Left Shift")) )
+        //{
+        //    //run       
+        //    SetRotation();
+        //}
+        //else
+        //{
+        //    //walk
+        //    SetRotation();
+        //}
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetRotation()
     {
-        
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(hor, 0, ver).normalized;
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            if (targetAngle != (Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y))
+            {
+                changed = true;
+                StopAllCoroutines();
+            }
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+
+            if (done || changed)
+            {
+                changed = false;
+                done = false;
+                StartCoroutine(LerpRotation(Quaternion.Euler(0f, targetAngle, 0f), 0.25f));
+            }
+
+        }
+        moveVector = new Vector3(direction.x, verticalVelosity, direction.z);
+    }
+
+    private void Move(float speed)
+    {
+
+        float hor = Input.GetAxisRaw("Horizontal");
+        float ver = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(hor, 0, ver).normalized;
+
+
+        if (direction.magnitude >= 0.1f)
+        {
+            targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            transform.position += moveDir.normalized * speed * Time.deltaTime;
+        }
+        moveVector = new Vector3(direction.x, verticalVelosity, direction.z); 
+        StartCoroutine(LerpRotation(Quaternion.Euler(0f, targetAngle, 0f), 0.25f));
+        SetRotation();
+
+        //if(PickUp.heldItem != null)
+        //{
+        //    Vector3 pos = PickUp.heldItem.transform.position;
+        //    pos = new Vector3(pos.x, pos.y + verticalVelosity, pos.z);
+        //    PickUp.heldItem.transform.position = pos;
+        //    transform.position += moveVector;
+        //}
+    }
+
+    private void Gravity()
+    {
+        transform.position += new Vector3(0, -1f * Time.deltaTime, 0);
+    }
+
+    IEnumerator LerpRotation(Quaternion endRotation, float duration)
+    {
+
+        float time = 0;
+        Quaternion startRotation = transform.rotation;
+        while (time < duration)
+        {
+
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.rotation = endRotation;
+        done = true;
     }
 }
+
+
