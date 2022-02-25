ee411ad767e682c6f4b143643b7ea4cbb82dd380
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2022-02-25 21:09:16 +0100

    Child Controller Start

---
+++ SoulDevour/SoulDevour/Assets/Amy/Scripts/Enemies/ChildController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildController : MonoBehaviour
+{
+
+    public bool playerInRoom;
+    public GameObject playerTarget;
+    public bool childMove = true;
+
+    public float radius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //playerTarget = GameObject.FindGameObjectWithTag("TargetPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = 10 * Time.deltaTime;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        foreach (Collider collider in colliders)
+        {
+            if (collider.transform.CompareTag("Player"))
+            {
+                childMove = false;
+                Debug.Log("Yass");
+            }
+           
+
+            if (childMove)
+            {
+                playerTarget = GameObject.FindGameObjectWithTag("Player");
+                gameObject.transform.position = Vector3.MoveTowards(transform.position, playerTarget.transform.position, speed);
+            }
+        }
+
+        //if (Vector3.Distance(transform.position, playerTarget.transform.position) < 3)
+        //{
+        //    speed = 0;
+        //    transform.position += Vector3.zero;
+        //    playerTarget.transform.position *= -1;
+        //}
+        //else
+        //{
+        //    gameObject.transform.position = Vector3.MoveTowards(transform.position, playerTarget.transform.position, speed);
+        //}
+    }
+
+    private void OnDrawGizmos()
+    {
+        Color color = Color.magenta;
+        Gizmos.DrawWireSphere(gameObject.transform.position, radius);
+    }
+}
