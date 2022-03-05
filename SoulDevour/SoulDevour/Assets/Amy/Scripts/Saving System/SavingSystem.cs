0c4c8959ec6feea84b26ce727b3dd23d5eb42b0a
Aithusa1 <77977074+Aithusa1@users.noreply.github.com>
2022-03-05 11:21:54 +0100

    Saving System

--- SoulDevour/SoulDevour/Assets/Amy/Scripts/Saving System/SavingSystem.cs
+++ SoulDevour/SoulDevour/Assets/Amy/Scripts/Saving System/SavingSystem.cs
@@ -1,23 +1,78 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Data
+{
+   
+
+    public Scene activeScene;
+    public float health;
+    public string levelSeed;
+    public List<ItemBase> itemsEquipped = new List<ItemBase>();
+
+    
+}
 
 public class SavingSystem : MonoBehaviour
 {
-    public SavingObject saveObj;
+    public GameObject player;
+    public GameObject generator;
 
-    private void Update()
+    public Data gameSaving = new Data();
+
+    private void Start()
     {
         
     }
 
-    public void Save() // op een button UI 
+    public void LoadButton()
     {
-       
+        Load();
     }
 
-    public void Load() // op een  button UI
+    public void SaveButton()
     {
+        Save();
+    }
+
+    public void Save()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+      
+
+        gameSaving.activeScene = scene;
+        gameSaving.health = player.GetComponent<MovPlayer>().calculatedHealth;
+        gameSaving.levelSeed = generator.GetComponent<FloorGenerator>().seed;
+        gameSaving.itemsEquipped = player.GetComponent<MovPlayer>().items;
+      
+        string json = JsonUtility.ToJson(gameSaving);
+        File.WriteAllText(Application.dataPath + "/saveFile.json", json);
 
     }
+
+    public void Load()
+    {
+
+
+        string json = File.ReadAllText(Application.dataPath + "/saveFile.json");
+        Data loadedData = JsonUtility.FromJson<Data>(json);
+
+        
+        // alleen dit nog laten werken ingame ! :) 
+        Debug.Log(loadedData.health);
+        Debug.Log(loadedData.levelSeed);
+        Debug.Log(loadedData.itemsEquipped);
+        Debug.Log(loadedData.activeScene);
+    }
+
+   
+
+    
+
+   
 }
