7e2419505e7e8f5cf669ee9b9b87b8614528d1f8
Lithuanianguy <69257029+Lithuanianguy@users.noreply.github.com>
2022-03-28 16:01:50 +0200

    Transition Scene

---
+++ SoulDevour/SoulDevour/Assets/Davido/UI/UI Anim/LevelLoader.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+}
