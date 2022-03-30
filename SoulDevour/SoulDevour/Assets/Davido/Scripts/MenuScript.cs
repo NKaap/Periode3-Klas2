1b3f7dcfcba1403b0b58733de54ed0fb38cc8b38
Lithuanianguy <69257029+Lithuanianguy@users.noreply.github.com>
2022-03-30 15:49:34 +0200

    changes in UI

--- SoulDevour/SoulDevour/Assets/Davido/Scripts/MenuScript.cs
+++ SoulDevour/SoulDevour/Assets/Davido/Scripts/MenuScript.cs
@@ -11,27 +11,44 @@ public class MenuScript : MonoBehaviour
     public Dropdown dropdown;
     public SceneRenderPipeline[] qualityLevels;
     public AudioMixer audioMixer;
+
+
+    //change quality
     void Start()
     {
         dropdown.value = QualitySettings.GetQualityLevel();
     }
 
-    //Scene's openen
-    public void LoadLevel()
+    
+
+    //load MainMenu
+    public void MainMenu()
     {
-        SceneManager.LoadScene("Amy's Lobby Test Scene");
+        SceneManager.LoadScene("Main");
     }
+
+
+    //quit
     public void QuitGame()
     {
         Application.Quit();
     }
 
+
+    public void AmyTest()
+    {
+        SceneManager.LoadScene("Amy's Lobby Test Scene");
+    }
+  
+
+    //fullscreen
     public void FullScreen(bool isFullScreen)
     {
         Screen.fullScreen = isFullScreen;
         Debug.Log("eee");
     }
 
+    //Quality changes
 
     public void SetQuality(int qualityIndex)
     {
@@ -45,23 +62,34 @@ public class MenuScript : MonoBehaviour
         print("heehee");
     }
 
-    public void MainMenu()
-    {
-        SceneManager.LoadScene("Main");
-    }
     
     
+    
+    //volume
+
     public void SetVolume(float volume)
     {
         print("eee");
         audioMixer.SetFloat("Volume", volume);
     }
     
-    
+  
+
+   
+
+  // Game Over Contineu
+
+    public void Con()
+    {
+        SceneManager.LoadScene("Amy's Lobby Test Scene");
+    }
+
+    //Transition
+
+
     public Animator transition;
 
     public float transitionTime = 1f;
-
 
     IEnumerator LoadLevel(int levelIndex)
     {
@@ -72,13 +100,17 @@ public class MenuScript : MonoBehaviour
         SceneManager.LoadScene(levelIndex);
     }
 
+
     public void ClickButoon()
     {
-        
+
         LoadNextLevel();
     }
     public void LoadNextLevel()
     {
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
+
+
+
 }
